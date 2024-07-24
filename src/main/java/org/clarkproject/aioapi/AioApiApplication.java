56a4faa53ae5bf6cff26a03f72fe275bbca3607c
clark.liu <dmc950213@gmail.com>
2024-07-24 10:19:22 +0900

    initial Create

---
+++ src/main/java/org/clarkproject/aioapi/AioApiApplication.java
@@ -0,0 +1,13 @@
+package org.clarkproject.aioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AioApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AioApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/clarkproject/aioapi/AioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.clarkproject.aioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AioApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
